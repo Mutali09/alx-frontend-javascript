07a726be97417809cc7bb5b520d74b8502070826
Mutali09 <nelsonsimiyu9.com>
2024-05-31 01:26:01 -0700

    changes

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,8 +1,8 @@
 export default function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-      // Simulate an asynchronous operation, e.g., fetching data from an API
-      setTimeout(() => {
-        resolve('Success');
-      }, 1000);
-    });
-  }
+  return new Promise((resolve) => {
+    // Simulate an asynchronous operation, e.g., fetching data from an API
+    setTimeout(() => {
+      resolve('Success');
+    }, 1000);
+  });
+}
